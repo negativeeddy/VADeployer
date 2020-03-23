4ef49f4d0caf340f3bf906172b0b5dc7dfc2870d
Ben Williams <benwilli@microsoft.com>
2020-03-23 11:24:22 -0500

    more name fixes

--- .devcontainer/setup.ps1
+++ .devcontainer/setup.ps1
@@ -1,12 +1,12 @@
 # clone the latest sample
 $workspaceDir = (Get-ChildItem Env:PWD).Value
 
-git clone https://github.com/microsoft/botframework-solutions.git $workspace/va_src
+git clone https://github.com/microsoft/botframework-solutions.git $workspaceDir/va_src
 
-copy-item -Path $workspace/va_src/samples/csharp/assistants/virtual-assistant -Destination $workspace/va -Recurse
+copy-item -Path $workspaceDir/va_src/samples/csharp/assistants/virtual-assistant -Destination $workspaceDir/va -Recurse
 
 # update the sample scripts to run the locally installed dispatch tool
-cd $workspace/va/VirtualAssistantSample/Deployment/Scripts
+cd $workspaceDir/va/VirtualAssistantSample/Deployment/Scripts
 
 (Get-Content -Path './deploy_cognitive_models.ps1') -replace 'dispatch create' ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create' | Set-Content './deploy_cognitive_models.ps1'
 (Get-Content -Path './deploy_cognitive_models.ps1') -replace 'dispatch init'   ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'   | Set-Content './deploy_cognitive_models.ps1'
