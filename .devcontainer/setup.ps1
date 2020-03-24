66d14229395bbd4e8f19f0887006c670f17b798a
Ben Williams <benwilli@microsoft.com>
2020-03-24 09:34:59 -0500

    make script more readable

--- .devcontainer/setup.ps1
+++ .devcontainer/setup.ps1
@@ -1,3 +1,9 @@
+function ReplaceInFile {
+    Param ([string] $filename, [string] $oldText, [string] $newText)
+    Write-Host("filename = $filename")
+    (Get-Content -Path $filename) -replace "$oldText", "$newText" | Set-Content $filename
+}
+
 # clone the latest sample
 $workspaceDir = (Get-ChildItem Env:PWD).Value
 
@@ -6,17 +12,18 @@ git clone https://github.com/microsoft/botframework-solutions.git $workspaceDir/
 copy-item -Path $workspaceDir/va_src/samples/csharp/assistants/virtual-assistant -Destination $workspaceDir/va -Recurse
 
 # update the sample scripts to run the locally installed dispatch tool
-cd $workspaceDir/va/VirtualAssistantSample/Deployment/Scripts
+$deployCogModelScript = "$workspaceDir/va/VirtualAssistantSample/Deployment/Scripts/deploy_cognitive_models.ps1"
+ReplaceInFile $deployCogModelScript 'dispatch create' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create'
+ReplaceInFile $deployCogModelScript 'dispatch init' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'
+ReplaceInFile $deployCogModelScript 'dispatch add' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll add'
 
-(Get-Content -Path './deploy_cognitive_models.ps1') -replace 'dispatch create' ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create' | Set-Content './deploy_cognitive_models.ps1'
-(Get-Content -Path './deploy_cognitive_models.ps1') -replace 'dispatch init'   ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'   | Set-Content './deploy_cognitive_models.ps1'
-(Get-Content -Path './deploy_cognitive_models.ps1') -replace 'dispatch add'    ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll add'    | Set-Content './deploy_cognitive_models.ps1'
+$updateCogModelScript = "$workspaceDir/va/VirtualAssistantSample/Deployment/Scripts/update_cognitive_models.ps1"
+ReplaceInFile $updateCogModelScript 'dispatch create' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create'
+ReplaceInFile $updateCogModelScript 'dispatch init' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'
+ReplaceInFile $updateCogModelScript 'dispatch add' 'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll add'
 
-(Get-Content -Path './update_cognitive_models.ps1') -replace 'dispatch create' ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll create' | Set-Content './update_cognitive_models.ps1'
-(Get-Content -Path './update_cognitive_models.ps1') -replace 'dispatch init'   ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll init'   | Set-Content './update_cognitive_models.ps1'
-(Get-Content -Path './update_cognitive_models.ps1') -replace 'dispatch add'    ,'dotnet /node_modules/botdispatch/bin/netcoreapp2.1/Dispatch.dll add'    | Set-Content './update_cognitive_models.ps1'
-
-(Get-Content -Path './publish.ps1') -replace 'bin\Release\netcoreapp3.0'    ,'bin\release\netcoreapp3.0'    | Set-Content './publish.ps1'
+$publishScript = "$workspaceDir/va/VirtualAssistantSample/Deployment/Scripts/publish.ps1"
+ReplaceInFile $publishScript 'bin\\Release\\netcoreapp3.0' 'bin\\release\\netcoreapp3.0'
 
 # prevent the telemetry prompt the first time bf cli is used
 "Y" | bf
